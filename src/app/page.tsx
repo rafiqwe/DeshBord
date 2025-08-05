5a2785c4527e0e624ba81da0b52880a0f3793141
rafiqwe <rmlmanik502@gmail.com>
2025-08-05 19:05:25 +0600

    Change some line of code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "@/components/ui/input-otp";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center gap-5 flex-col">
       <h1 className="text-3xl font-bold capitalize">muhammad rabbi ☠️</h1>
-      <Button variant='destructive' size='lg'>Click Here</Button>
+    <Button variant='destructive' size='lg'> Click Me </Button>
+     <InputOTP maxLength={6}>
+      <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator/>
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+    </InputOTP>
     </div>
   );
 }
